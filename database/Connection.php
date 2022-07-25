7c2586a4327f7ccf348f96f68ddca338a1c6a4ea
binayak giri <binayakgiri@binayaks-MBP.mshome.net>
2022-07-25 07:02:09 +0545

    Connection fixes

--- database/Connection.php
+++ database/Connection.php
@@ -4,7 +4,7 @@ class Connection
 {
   public static function make()
   {
-    $host     = "localhost";//Ip of database, in this case my host machine    
+    $host     = "localhost";//Ip of database, in this case my host    
     $user     = "root";	//Username to use
     $pass     = "";//Password for that user
     $dbname   = "task";//Name of the database
